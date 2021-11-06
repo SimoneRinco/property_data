84a50d5c2ce827bd9dd37544866c17a73fc30db8
Simone Rinco <SRinco@slb.com>
2021-11-06 11:39:32 +0000

    added date logged to sold prices

--- pdata/db/sold_prices.py
+++ pdata/db/sold_prices.py
@@ -18,7 +18,8 @@ class SoldPricesDb(object):
                     'bedrooms',
                     'ptype',
                     'date_sold',
-                    'price_sold'
+                    'price_sold',
+                    'date_logged'
                     ])
 
   def __init__(self, db_name):
@@ -30,7 +31,7 @@ class SoldPricesDb(object):
 
 
   @staticmethod
-  def row_from_sold_prices_raw_item(postcode_district, raw_item):
+  def row_from_sold_prices_raw_item(postcode_district, date_logged, raw_item):
     if raw_item.coordinates is None:
       lat, lng = None, None
     else:
@@ -53,7 +54,9 @@ class SoldPricesDb(object):
       bedrooms=raw_item.bedrooms,
       ptype=db_utils.ptype2int(raw_item.ptype),
       date_sold=db_utils.date2int(raw_item.date),
-      price_sold=raw_item.price)
+      price_sold=raw_item.price,
+      date_logged=db_utils.date2int(date_logged)
+      )
 
 
   def create_table(self):
@@ -69,16 +72,17 @@ class SoldPricesDb(object):
         type                  INTEGER NOT NULL,
         date_sold             INTEGER NOT NULL,
         price_sold            INTEGER NOT NULL,
+        date_logged           INTEGER NOT NULL,
         PRIMARY KEY (postcode, address, date_sold)
       )
         ''')
 
-  def update(self, postcode_district, sold_prices):
+  def update(self, postcode_district, date_logged, sold_prices):
     """sold_prices: sold prices for the specified postcode district and and date (SoldPrices.Data)
     """
 
     self.create_table()
-    r_db = [SoldPricesDb.row_from_sold_prices_raw_item(postcode_district, r) for r in sold_prices.raw_items]
+    r_db = [SoldPricesDb.row_from_sold_prices_raw_item(postcode_district, date_logged, r) for r in sold_prices.raw_items]
 
     new_sold = 0
     for r in r_db:
@@ -92,23 +96,48 @@ class SoldPricesDb(object):
         :bedrooms,
         :ptype,
         :date_sold,
-        :price_sold
+        :price_sold,
+        :date_logged
         )      
         """, r._asdict())
       new_sold += cur.rowcount
 
-    print (f"{new_sold} new data points")
+    print (f"{postcode_district}, {date_logged}: {new_sold} new data points")
+
+
+def fill_database(postcode, dates):
+
+  for d in dates:
+    with open(os.path.join('test_datasets', 'sold_prices', postcode, f'{d.isoformat()}.json')) as f:
+      p = SoldPrices.load(f)
+
+    db = SoldPricesDb("test_db/propertydata.db")
+    db.connect()
+    db.update(postcode, d, p)
+    db.connection.commit()
+    db.connection.close()
+
+def update_today(postcode):
+  dates = [datetime.date.today()]
+  fill_database(postcode, dates)
+
+def update_all(postcode):
+
+  dates = os.listdir(os.path.join('test_datasets', 'sold_prices', postcode))
+  dates = [date.fromisoformat(d.split('.')[0]) for d in dates]
+  dates.sort()
+  fill_database(postcode, dates)
 
 
 if __name__ == '__main__':
-  d = date.today()
-  postcode = "RG14"
-  with open(os.path.join('test_datasets', 'sold_prices', postcode, f'{d.isoformat()}.json')) as f:
-    p = SoldPrices.load(f)
-
-  db = SoldPricesDb("test_db/propertydata.db")
-  db.connect()
-  db.update(postcode, p)
-  db.connection.commit()
-  db.connection.close()
-
+  postcodes = [
+    "OX1", "OX2", "OX3", "OX4", # Oxford
+    "OX13", "OX14", # Abingdon
+    "RG14", # Newbury
+    "RG18", "RG19", # Thatcham
+    "RG1", "RG2", "RG4", "RG5", "RG6", # Reading
+    "RG9", # Henley on Thames
+    ]
+  #update_today(postcode)
+  for p in postcodes:
+    update_all(p)
